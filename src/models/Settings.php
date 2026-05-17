3637b3343099ce312094477d95a640f14e160352
Arjan <info@skoften.net>
2026-05-17 15:01:19 +0200

    Fix settings validation for transcoder paths

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -286,7 +286,7 @@ class Settings extends Model
                 unset($config['transcoderPath']);
             }
             if (isset($config['transcoderUrl'])) {
-                $config['$transcoderUrls']['default'] = $config['transcoderUrl'];
+                $config['transcoderUrls']['default'] = $config['transcoderUrl'];
                 unset($config['transcoderUrl']);
             }
         }
@@ -308,8 +308,6 @@ class Settings extends Model
             ['ffprobePath', 'required'],
             ['ffprobeOptions', 'string'],
             ['ffprobeOptions', 'safe'],
-            ['transcoderPath', 'string'],
-            ['transcoderPath', 'required'],
             ['transcoderPaths', ArrayValidator::class],
             ['transcoderPaths', 'required'],
             ['transcoderUrls', ArrayValidator::class],
